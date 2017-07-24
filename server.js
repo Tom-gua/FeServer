88cb2af7be6cfa7142916565b77dcd9672555f1e
liteng <liteng@wafersystems.com>
2017-07-24 17:20:59 +0800

    增加了转发请求的代码

--- server.js
+++ server.js
@@ -58,10 +58,59 @@ const httpOptions = (request) => {
     options.method = request.method
     return options
 }
-// 我们理论上只转发 api　的请求，所有符合　／api的请求都会被转发
+// 理论上只转发 api　的请求，所有符合　／api的请求都会被转发
 app.all('/api/*', (request, response) => {
     // 1.根据本地请求资源，修改为转发的请求资源
     const options = httpOptions(request)
+    const client = clientByProtocol(options.protocol)
+    // http.request 返回一个请求对象, 这个请求对象会把数据发送到 api server
+    const r = client.request(options, (res) => {
+        // res.statusCode 是 api server 返回的状态码
+        // 保持 express response 的状态码和 api server 的状态码一致
+        // 避免出现返回 304, 导致 response 出错
+        response.status(res.statusCode)
+        // 回调里的 res 是 api server 返回的响应
+        // 将响应的 headers 原样设置到 response(这个是 express 的 response) 中
+        Object.keys(res.headers).forEach((k) => {
+            const v = res.headers[k]
+            response.setHeader(k, v)
+        })
+        // 接收 api server 的响应时, 会触发 data 事件
+        res.on('data', (data) => {
+            // express 的 response 对象是 http 对象的加强版, 可以调用 http 对象的方法
+            // write 是 http 对象的方法, 服务器用来发送响应数据的
+            log('debug data', data.toString('utf8'))
+            response.write(data)
+        })
+        // api server 的数据接收完成后, 会触发 end 事件
+        res.on('end', () => {
+            log('debug end')
+            // api server 发送完数据之后, express 也告诉客户端发送完数据
+            response.end()
+        })
+
+        // 响应发送错误
+        res.on('error', () => {
+            console.error(`error to request: ${request.url}`)
+        })
+    })
+    // 发往 api server 的请求遇到问题
+    r.on('error', (error) => {
+        console.error(`请求 api server 遇到问题: ${request.url}`, error)
+    })
+
+    if (options.method !== 'GET') {
+        // request.body 是浏览器发送过来的数据,
+        // 如果不是 GET 方法, 说明 request.body 有内容,
+        // 转成 JSON 字符串之后发送到 api server
+        const body = JSON.stringify(request.body)
+        log('debug body', body, typeof body)
+        // 把 body 里的数据发送到 api server
+        r.write(body)
+    }
+
+    // 结束发送请求
+    r.end()
 })
 const run = (port=3000, host='') => {
     const server = app.listen(port, host, () => {
